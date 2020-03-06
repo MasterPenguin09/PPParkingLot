25dc13f2954834e2064f22e5b83435c484811765
Nathalie Pfau <nana.pfau@gmail.com>
2020-03-06 15:38:33 -0300

    Ajustes mais Enum de tipo de veiculo

--- DataTransferObject/VehicleDTO.cs
+++ DataTransferObject/VehicleDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataTransferObject.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,10 +12,7 @@ namespace DataTransferObject
         public int ID { get; set; }
        // public int IDCliente { get; set; }
         public string CarBoard { get; set; }
-
-        public  BrandDTO Brand { get; set; }
-        public  int BrandID { get; set; }
-
+        public EVehicleType Type { get; set; }
         public ModelDTO Model { get; set; }
         public int ModelID { get; set; }
 
