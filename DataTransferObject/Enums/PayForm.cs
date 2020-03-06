e300db7eae67a5952865927888a3ecc88fcfef67
Jonathan Oliveira <jonathan-jona1@hotmail.com>
2020-03-06 14:13:22 -0300

    LocatioDTO e enum de PayForm

---
+++ DataTransferObject/Enums/PayForm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTransferObject.Enums
+{
+   public enum PayForm
+    {
+        CartaoD,
+        CartaoC,
+        Dinheiro
+
+
+    }
+}
---
+++ DataTransferObject/LocationDTO.cs
@@ -0,0 +1,18 @@
+﻿using DataTransferObject.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTransferObject
+{
+    class LocationDTO
+    {
+        public int ID { get; set; }
+        public DateTime EntryTime { get; set; }
+        public DateTime ExitTime { get; set; }
+        public PayForm PayForm { get; set; }
+
+    }
+}
