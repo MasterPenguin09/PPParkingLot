7724d970983455d18a3d24e73377e3424f4c7129
gabrielarnovoltolini1945@gmail.com <gabrielarnovoltolini1945@gmail.com>
2020-03-06 15:01:28 -0300

    Application Flow Control (Response e DataResponse) Prontos

---
+++ Common/FlowControl/DataResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.FlowControl
+{
+    class DataResponse<T> : Response
+    {
+        public DataResponse()
+        {
+
+        }
+
+        public DataResponse(List<T> data)
+        {
+            Data = data;
+        }
+
+        public List<T> Data { get; set; } 
+    }
+}
---
+++ Common/FlowControl/Response.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.FlowControl
+{
+    public class Response
+    {
+       
+        public Response(bool success, List<string> errors)
+        {
+            Success = success;
+            Errors = errors;
+        }
+        public Response()
+        {
+
+        }
+
+        public bool Success { get; set; }
+        public List<string> Errors { get; set; }
+
+        public string GetErrors() 
+        {
+            return this.Errors.ToString();
+        }
+
+    }
+}
