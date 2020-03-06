6d917d45b846529c00e8f249466bfee7c626368e
gabrielarnovoltolini1945@gmail.com <gabrielarnovoltolini1945@gmail.com>
2020-03-06 14:21:39 -0300

    UserPattern Pronto

---
+++ Common/UserPattern.cs
@@ -0,0 +1,39 @@
+﻿using Common.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class UserPattern
+    {
+        public UserPattern(string name, string cPF, string number, DateTime birthDate, string passWord, string email, DateTime systemEntranceDate, DateTime systemExitDate, EAccessLevel accessLevel, bool isActive)
+        {
+            Name = name;
+            CPF = cPF;
+            Number = number;
+            BirthDate = birthDate;
+            PassWord = passWord;
+            Email = email;
+            SystemEntranceDate = systemEntranceDate;
+            SystemExitDate = systemExitDate;
+            AccessLevel = accessLevel;
+            IsActive = isActive;
+        }
+
+        public UserPattern() { }
+
+        public string Name { get; set; }
+        public string CPF { get; set; }
+        public string Number { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string PassWord { get; set; }
+        public string Email { get; set; }
+        public DateTime SystemEntranceDate { get; set; }
+        public DateTime SystemExitDate { get; set; }
+        public EAccessLevel AccessLevel { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
