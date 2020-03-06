3feb13f4094421e98b70931834539748016f4155
Nathalie Pfau <nana.pfau@gmail.com>
2020-03-06 14:51:54 -0300

    Criei o VeiculoDTO

---
+++ DataTransferObject/VehicleDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTransferObject
+{
+   public class VehicleDTO
+    {
+        public int ID { get; set; }
+       // public int IDCliente { get; set; }
+        public string CarBoard { get; set; }
+        //public string Brand { get; set; }
+        //public string Model { get; set; }
+
+    }
+}
