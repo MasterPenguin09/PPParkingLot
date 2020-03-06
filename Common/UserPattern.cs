8f9a9d1ee75be246c019076934cf02fb8c6db2ee
arthur.martins@hbsis.com.br <arthur.martins@hbsis.com.br>
2020-03-06 16:42:29 -0300

    i

--- Common/UserPattern.cs
+++ Common/UserPattern.cs
@@ -9,14 +9,14 @@ namespace Common
 {
     public class UserPattern
     {
-        public UserPattern(int id, string name, string cPF, string number, DateTime birthDate, string passWord, string email, DateTime systemEntranceDate, DateTime systemExitDate, EAccessLevel accessLevel, bool isActive)
+        public UserPattern(int id, string name, string cPF, string number, DateTime birthDate, string password, string email, DateTime systemEntranceDate, DateTime systemExitDate, EAccessLevel accessLevel, bool isActive)
         {
             ID = id;
             Name = name;
             CPF = cPF;
             Number = number;
             BirthDate = birthDate;
-            PassWord = passWord;
+            Password = password;
             Email = email;
             SystemEntranceDate = systemEntranceDate;
             SystemExitDate = systemExitDate;
@@ -31,7 +31,7 @@ namespace Common
         public string CPF { get; set; }
         public string Number { get; set; }
         public DateTime BirthDate { get; set; }
-        public string PassWord { get; set; }
+        public string Password { get; set; }
         public string Email { get; set; }
         public DateTime SystemEntranceDate { get; set; }
         public DateTime SystemExitDate { get; set; }
