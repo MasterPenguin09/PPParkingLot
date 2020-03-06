cc8556bfcda6dd6d61011c08dd124fbe37f833ed
arthur.martins@hbsis.com.br <arthur.martins@hbsis.com.br>
2020-03-06 15:16:57 -0300

    EmployeeDTO e ID no UserPattern

--- DataTransferObject/EmployeeDTO.cs
+++ DataTransferObject/EmployeeDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,11 @@ using System.Threading.Tasks;
 
 namespace DataTransferObject
 {
-    class EmployeeDTO
+    class EmployeeDTO : UserPattern
     {
+        
+        public double Wage { get; set; }
+
+
     }
 }
