9fabcd4e73d423cdd43ec39a7f44fb47f1fb7c99
Gabriel Voltolini <gabrielarnovolyolini1945@gmail.com>
2020-03-07 23:53:38 -0300

    UserPattern foi movido de "Common" -> DTO.ComplexTypes

--- DataTransferObject/EmployeeDTO.cs
+++ DataTransferObject/EmployeeDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataTransferObject.ComplexTypes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace DataTransferObject
 {
-    public class EmployeeDTO
+    public class EmployeeDTO : UserPattern
     {
         public EmployeeDTO(double wage)
         {
