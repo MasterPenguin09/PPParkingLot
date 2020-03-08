858d1f72e889536aeec2ae3ec2274da19e591d50
Gabriel Voltolini <gabrielarnovolyolini1945@gmail.com>
2020-03-07 23:41:58 -0300

    Repositórios criados no DAL

---
+++ DataAccessLayer/Repositories(EFCore)/ModelRepository.cs
@@ -0,0 +1,13 @@
+﻿using DataAccessLayer.Interfaces_EFCore_;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories_EFCore_
+{
+    public class ModelRepository : IModelRepository
+    {
+    }
+}
