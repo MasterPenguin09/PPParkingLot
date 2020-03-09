5fbb613a1a98f65cb591b2e2283f469066761a78
gabrielarnovoltolini1945@gmail.com <gabrielarnovoltolini1945@gmail.com>
2020-03-09 17:31:06 -0300

    EmployeeService Delete Pronto

--- BusinessLogicalLayer/Impl/EmployeeService.cs
+++ BusinessLogicalLayer/Impl/EmployeeService.cs
@@ -1,7 +1,9 @@
 ﻿using BusinessLogicalLayer.Interfaces;
+using BusinessLogicalLayer.Validators;
 using Common.FlowControl;
 using DataAccessLayer.Interfaces_EFCore_;
 using DataTransferObject;
+using FluentValidation.Results;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,42 +20,58 @@ namespace BusinessLogicalLayer.Impl
             this._iEmployeeRepository = iEmployeeRep;
         }
 
-        public Task<Response> Delete(EmployeeDTO employee)
+        public async Task<Response> Delete(EmployeeDTO employee)
+        {
+            Response response = new Response();
+            EmployeeValidator validate = new EmployeeValidator();
+            ValidationResult result = validate.Validate(employee);
+
+            if (!result.IsValid)
+            {
+                foreach (var failure in result.Errors)
+                {
+                    response.Errors.Add("Property " + failure.PropertyName + " failed validation. Error was: " + "(" + failure.ErrorMessage + ")");
+                }
+
+                return response;
+            }
+            else
+            {
+                return await _iEmployeeRepository.Delete(employee);
+            }
+        }
+
+        public async Task<Response> Disable(EmployeeDTO employee)
         {
             throw new NotImplementedException();
         }
 
-        public Task<Response> Disable(EmployeeDTO employee)
+        public async Task<DataResponse<EmployeeDTO>> GetActives()
         {
             throw new NotImplementedException();
         }
 
-        public Task<DataResponse<EmployeeDTO>> GetActives()
+        public async Task<DataResponse<EmployeeDTO>> GetAll()
         {
             throw new NotImplementedException();
         }
 
-        public Task<DataResponse<EmployeeDTO>> GetAll()
+        public async Task<DataResponse<EmployeeDTO>> GetByID(int employeeID)
         {
             throw new NotImplementedException();
         }
 
-        public Task<DataResponse<EmployeeDTO>> GetByID(int employeeID)
+        public async Task<DataResponse<EmployeeDTO>> GetByName(string employeeName)
         {
             throw new NotImplementedException();
         }
 
-        public Task<DataResponse<EmployeeDTO>> GetByName(string employeeName)
+        public async Task<Response> Insert(EmployeeDTO employee)
         {
             throw new NotImplementedException();
         }
 
-        public Task<Response> Insert(EmployeeDTO employee)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<Response> Update(EmployeeDTO employee)
+        public async Task<Response> Update(EmployeeDTO employee)
         {
             throw new NotImplementedException();
         }
