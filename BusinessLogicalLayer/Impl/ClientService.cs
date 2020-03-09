575e182e113cf60c59bd2084722f8bd0caf39a0c
gabrielarnovoltolini1945@gmail.com <gabrielarnovoltolini1945@gmail.com>
2020-03-09 15:56:53 -0300

    Client validator pronto

--- BusinessLogicalLayer/Impl/ClientService.cs
+++ BusinessLogicalLayer/Impl/ClientService.cs
@@ -1,5 +1,7 @@
 ﻿using BusinessLogicalLayer.Interfaces;
+using Common.FlowControl;
 using DataAccessLayer.Interfaces_EFCore_;
+using DataTransferObject;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +16,46 @@ namespace BusinessLogicalLayer.Impl
         public ClientService(IClientRepository iClientRep)
         {
             this._iClientRepository = iClientRep;
+        }
+
+        public Task<Response> Delete(ClientDTO client)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response> Disable(ClientDTO client)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DataResponse<ClientDTO>> GetActives()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DataResponse<ClientDTO>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DataResponse<ClientDTO>> GetByID(int clientID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DataResponse<ClientDTO>> GetByName(string clientName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response> Insert(ClientDTO client)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response> Update(ClientDTO client)
+        {
+            throw new NotImplementedException();
         }
     }
 }
---
+++ BusinessLogicalLayer/Validators/ClientValidator.cs
@@ -0,0 +1,21 @@
+﻿using DataTransferObject;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicalLayer.Validators
+{
+    class ClientValidator : AbstractValidator<ClientDTO>
+    {
+        public ClientValidator()
+        {
+            RuleFor(c => c.BirthDate).LessThan(DateTime.Now);
+            RuleFor(c => c.Name).Length(3,64);
+            RuleFor(c => c.Name).NotEmpty();
+            RuleFor(c => c.AccessLevel).IsInEnum();
+        }
+    }
+}
