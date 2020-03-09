192de36ad2a89d51ff18d28a11753ee25791de6e
gabrielarnovoltolini1945@gmail.com <gabrielarnovoltolini1945@gmail.com>
2020-03-09 17:25:38 -0300

    EmployeeValidator pronto

---
+++ BusinessLogicalLayer/Validators/EmployeeValidator.cs
@@ -0,0 +1,22 @@
+﻿using DataTransferObject;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicalLayer.Validators
+{
+    class EmployeeValidator : AbstractValidator<EmployeeDTO>
+    {
+        public EmployeeValidator()
+        {
+            RuleFor(c => c.BirthDate).LessThan(DateTime.Now);
+            RuleFor(c => c.Name).Length(3, 64);
+            RuleFor(c => c.Name).NotEmpty();
+            RuleFor(c => c.AccessLevel).IsInEnum();
+        }
+     
+    }
+}
