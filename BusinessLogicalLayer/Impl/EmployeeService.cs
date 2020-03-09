3bb10b53b49a971d86cc82a46d0cd546b076bcb5
gabrielarnovoltolini1945@gmail.com <gabrielarnovoltolini1945@gmail.com>
2020-03-09 17:27:28 -0300

    Interface implementada ClientService

--- BusinessLogicalLayer/Impl/EmployeeService.cs
+++ BusinessLogicalLayer/Impl/EmployeeService.cs
@@ -1,5 +1,7 @@
 ﻿using BusinessLogicalLayer.Interfaces;
+using Common.FlowControl;
 using DataAccessLayer.Interfaces_EFCore_;
+using DataTransferObject;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +16,46 @@ namespace BusinessLogicalLayer.Impl
         public EmployeeService(IEmployeeRepository iEmployeeRep)
         {
             this._iEmployeeRepository = iEmployeeRep;
+        }
+
+        public Task<Response> Delete(EmployeeDTO employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response> Disable(EmployeeDTO employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DataResponse<EmployeeDTO>> GetActives()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DataResponse<EmployeeDTO>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DataResponse<EmployeeDTO>> GetByID(int employeeID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DataResponse<EmployeeDTO>> GetByName(string employeeName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response> Insert(EmployeeDTO employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response> Update(EmployeeDTO employee)
+        {
+            throw new NotImplementedException();
         }
     }
 }
