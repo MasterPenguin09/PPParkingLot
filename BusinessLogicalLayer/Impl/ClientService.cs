d3532f3f0561c11b0ca8106f1094da2c26f13bff
gabrielarnovoltolini1945@gmail.com <gabrielarnovoltolini1945@gmail.com>
2020-03-09 16:01:52 -0300

    Cient delete pronto

--- BusinessLogicalLayer/Impl/ClientService.cs
+++ BusinessLogicalLayer/Impl/ClientService.cs
@@ -1,7 +1,9 @@
 ﻿using BusinessLogicalLayer.Interfaces;
+using BusinessLogicalLayer.Validators;
 using Common.FlowControl;
 using DataAccessLayer.Interfaces_EFCore_;
 using DataTransferObject;
+using FluentValidation.Results;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,48 +14,64 @@ namespace BusinessLogicalLayer.Impl
 {
    public class ClientService : IClientService
     {
-        private IClientRepository _iClientRepository;
+        private  IClientRepository _iClientRepository;
         public ClientService(IClientRepository iClientRep)
         {
             this._iClientRepository = iClientRep;
         }
 
-        public Task<Response> Delete(ClientDTO client)
+        public async Task<Response> Delete(ClientDTO client)
+        {
+            Response response = new Response();
+            ClientValidator validate = new ClientValidator();
+            ValidationResult result = validate.Validate(client);
+
+            if (!result.IsValid)
+            {
+                foreach (var failure in result.Errors)
+                {
+                    response.Errors.Add("Property " + failure.PropertyName + " failed validation. Error was: " + "(" + failure.ErrorMessage + ")");
+                }
+
+                return response;
+            }
+            else
+            {
+                return await _iClientRepository.Delete(client);
+            }
+        }
+
+        public async Task<Response> Disable(ClientDTO client)
         {
             throw new NotImplementedException();
         }
 
-        public Task<Response> Disable(ClientDTO client)
+        public async Task<DataResponse<ClientDTO>> GetActives()
         {
             throw new NotImplementedException();
         }
 
-        public Task<DataResponse<ClientDTO>> GetActives()
+        public async Task<DataResponse<ClientDTO>> GetAll()
         {
             throw new NotImplementedException();
         }
 
-        public Task<DataResponse<ClientDTO>> GetAll()
+        public async Task<DataResponse<ClientDTO>> GetByID(int clientID)
         {
             throw new NotImplementedException();
         }
 
-        public Task<DataResponse<ClientDTO>> GetByID(int clientID)
+        public async Task<DataResponse<ClientDTO>> GetByName(string clientName)
         {
             throw new NotImplementedException();
         }
 
-        public Task<DataResponse<ClientDTO>> GetByName(string clientName)
+        public async Task<Response> Insert(ClientDTO client)
         {
             throw new NotImplementedException();
         }
 
-        public Task<Response> Insert(ClientDTO client)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<Response> Update(ClientDTO client)
+        public async Task<Response> Update(ClientDTO client)
         {
             throw new NotImplementedException();
         }
