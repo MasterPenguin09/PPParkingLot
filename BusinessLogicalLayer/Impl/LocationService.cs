f9197ca7c8c4ee0d4de2ad8b82aaf17841e20c96
mooh_oli <morganaoliveira59@gmail.com>
2020-03-09 14:09:40 -0300

    validators

--- BusinessLogicalLayer/Impl/LocationService.cs
+++ BusinessLogicalLayer/Impl/LocationService.cs
@@ -1,5 +1,8 @@
 ﻿using BusinessLogicalLayer.Interfaces;
+using Common.FlowControl;
 using DataAccessLayer.Interfaces_EFCore_;
+using DataTransferObject;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,7 +11,7 @@ using System.Threading.Tasks;
 
 namespace BusinessLogicalLayer.Impl
 {
-   public class LocationService : ILocationSevice
+    public class LocationService :  ILocationSevice
     {
         private ILocationRepository _iLocationRepository;
         public LocationService(ILocationRepository iLocationRep)
@@ -16,5 +19,46 @@ namespace BusinessLogicalLayer.Impl
             this._iLocationRepository = iLocationRep;
         }
 
+
+        public  Task<Response> Delete(LocationDTO location)
+        {
+
+
+        }
+
+        public Task<Response> Disable(LocationDTO location)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DataResponse<LocationDTO>> GetActives()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DataResponse<LocationDTO>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DataResponse<LocationDTO>> GetByID(int locationID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DataResponse<LocationDTO>> GetByValue(double locationValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response> Insert(LocationDTO location)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response> Update(LocationDTO location)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- DataAccessLayer/Repositories(EFCore)/LocationRepository.cs
+++ DataAccessLayer/Repositories(EFCore)/LocationRepository.cs
@@ -1,4 +1,6 @@
-﻿using DataAccessLayer.Interfaces_EFCore_;
+﻿using Common.FlowControl;
+using DataAccessLayer.Interfaces_EFCore_;
+using DataTransferObject;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +11,44 @@ namespace DataAccessLayer.Repositories_EFCore_
 {
     public class LocationRepository : ILocationRepository
     {
+        public Task<Response> Delete(LocationDTO location)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response> Disable(LocationDTO location)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DataResponse<LocationDTO>> GetActives()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DataResponse<LocationDTO>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DataResponse<LocationDTO>> GetByID(int locationID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DataResponse<LocationDTO>> GetByValue(double locationValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response> Insert(LocationDTO location)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response> Update(LocationDTO location)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
