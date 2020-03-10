4f156ecc9d3a67a3b8464d54cb1438beedb3f41a
gabrielarnovoltolini1945@gmail.com <gabrielarnovoltolini1945@gmail.com>
2020-03-10 15:18:06 -0300

    ParkingSpotValidator pronto

---
+++ BusinessLogicalLayer/Validators/ParkingSpotValidator.cs
@@ -0,0 +1,22 @@
+﻿using DataTransferObject;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicalLayer.Validators
+{
+    class ParkingSpotValidator : AbstractValidator<ParkingSpotDTO>
+    {
+        public ParkingSpotValidator()
+        {
+            RuleFor(c => c.Type).IsInEnum();
+            RuleFor(c => c.ValuePerHour).Empty();
+            RuleFor(c => c.ValuePerHour).GreaterThan(0);
+            RuleFor(c => c.ID).Empty();
+            RuleFor(c => c.IsActive).Equal(true);
+        }
+    }
+}
