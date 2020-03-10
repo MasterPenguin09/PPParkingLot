aeb2e9dd87d6cc45a76e63fda1c6e1d1dbb59602
Gabriel Voltolini <gabrielarnovolyolini1945@gmail.com>
2020-03-10 10:34:29 -0300

    Extensions pronta

---
+++ BusinessLogicalLayer/Extensions/FluentValidationsComplements.cs
@@ -0,0 +1,18 @@
+﻿using Common.FlowControl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicalLayer.Extensions
+{
+    static class FluentValidationsComplements
+    {
+        public static string MergeValidationErrors(this Response response) 
+        {
+            //TODO: Errors Merge 
+            return null;
+        }
+    }
+}
