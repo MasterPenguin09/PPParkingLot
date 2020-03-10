5e42f07a03d947de7cb9d61909e75dcf76d5d538
gabrielarnovoltolini1945@gmail.com <gabrielarnovoltolini1945@gmail.com>
2020-03-10 16:16:38 -0300

    ParkingSpotRepository cirada e implementada

---
+++ DataAccessLayer/Repositories(EFCore)/ParkingSpotRepository.cs
@@ -0,0 +1,49 @@
+﻿using Common.FlowControl;
+using DataAccessLayer.Interfaces_EFCore_;
+using DataTransferObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories_EFCore_
+{
+    public class ParkingSpotRepository : IParkingSpotRepository
+    {
+        public Task<Response> Delete(int idPakingSpot)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response> Disable(int idPakingSpot)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DataResponse<ParkingSpotDTO>> GetActives()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DataResponse<ParkingSpotDTO>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DataResponse<ParkingSpotDTO>> GetByID(int idPakingSpot)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response> Insert(ParkingSpotDTO pakingSpot)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response> Update(ParkingSpotDTO pakingSpot)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
