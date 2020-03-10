f251be509e0332441c714ccc3d4d9fd7bb55e0f7
Gabriel Voltolini <gabrielarnovolyolini1945@gmail.com>
2020-03-10 10:11:25 -0300

    EmailValidatorExtension, CPFValidator Extension prontos

---
+++ BusinessLogicalLayer/Extensions/EmailValidator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BusinessLogicalLayer.Extensions
+{
+    static class EmailValidator
+    {
+        static Regex ValidEmailRegex = CreateValidEmailRegex();
+        private static Regex CreateValidEmailRegex()
+        {
+            string validEmailPattern = @"^(?!\.)(""([^""\r\\]|\\[""\r\\])*""|"
+                + @"([-a-z0-9!#$%&'*+/=?^_`{|}~]|(?<!\.)\.)*)(?<!\.)"
+                + @"@[a-z0-9][\w\.-]*[a-z0-9]\.[a-z][a-z\.]*[a-z]$";
+
+            return new Regex(validEmailPattern, RegexOptions.IgnoreCase);
+        }
+
+        internal static bool EmailIsValid(this string emailAddress)
+        {
+            bool isValid = ValidEmailRegex.IsMatch(emailAddress);
+
+            return isValid;
+        }
+    }
+}
