92529593195b12f68f4c604b5cd3e58e96f28ce4
gabrielarnovoltolini1945@gmail.com <gabrielarnovoltolini1945@gmail.com>
2020-03-10 15:53:32 -0300

    Construtor da locacao recirado para inclusãio do relacionamento vaga

--- DataTransferObject/LocationDTO.cs
+++ DataTransferObject/LocationDTO.cs
@@ -10,7 +10,7 @@ namespace DataTransferObject
 
     public class LocationDTO
     {
-        public LocationDTO(int iD, DateTime entryTime, DateTime exitTime, PayForm payForm, int value, VehicleDTO vehicle, int vehicleID)
+        public LocationDTO(int iD, DateTime entryTime, DateTime? exitTime, PayForm payForm, int value, VehicleDTO vehicle, int vehicleID, ParkingSpotDTO parkingSpot, int parkingSpotID)
         {
             ID = iD;
             EntryTime = entryTime;
@@ -19,6 +19,8 @@ namespace DataTransferObject
             Value = value;
             Vehicle = vehicle;
             VehicleID = vehicleID;
+            ParkingSpot = parkingSpot;
+            ParkingSpotID = parkingSpotID;
         }
         public LocationDTO()
         {
