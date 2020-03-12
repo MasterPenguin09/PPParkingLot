9f1ee0709db1092f751ae2532dd6e20f1d8bdb1f
gabrielarnovoltolini1945@gmail.com <gabrielarnovoltolini1945@gmail.com>
2020-03-12 17:30:51 -0300

    Calculate price comentado

--- BLL/Impl/LocationService.cs
+++ BLL/Impl/LocationService.cs
@@ -145,29 +145,52 @@ namespace BusinessLogicalLayer.Impl
                 return await _iLocationRepository.Update(location);
             }
         }
-        private Response CalculatePrice(int idLocation)
+
+        /// <summary>
+        /// Método que executa o "fechamento de vaga" (quando o cliente sai)
+        /// </summary>
+        /// <param name="idLocation"></param>
+        /// <returns></returns>
+        private DataResponse<LocationDTO> CalculatePrice(int idLocation)
         {
-            Response response = new Response();
-            Task<DataResponse<LocationDTO>> location = _iLocationRepository.GetByID(idLocation);
-            LocationDTO locacao = location.Result.Data.FirstOrDefault();
-            locacao.ExitTime = DateTime.Now;
-            TimeSpan tempoDeLocacao = locacao.ExitTime.Value.Subtract(locacao.EntryTime);
-            double valorLocacao = tempoDeLocacao.Hours * locacao.ParkingSpot.ValuePerHour;
-            locacao.Value = valorLocacao;
-            if (_iLocationRepository.Update(locacao).Result.Success)
+            DataResponse<LocationDTO> response = new DataResponse<LocationDTO>();
+
+            //Busca a locação pelo id
+            Task<DataResponse<LocationDTO>> temp_location = _iLocationRepository.GetByID(idLocation);
+
+            //Armazena a locação encontrada 
+            LocationDTO location = temp_location.Result.Data.FirstOrDefault();
+
+            //Adiciona a data de saída a locação 
+            location.ExitTime = DateTime.Now;
+
+            //Subtrai o valor de saída com o valor de entrada 
+            TimeSpan tempoDeLocacao = location.ExitTime.Value.Subtract(location.EntryTime);
+
+            //Pega a o valor subtraído em horas e multiplica pelo valor/hora da vaga
+            double valorLocacao = tempoDeLocacao.Hours * location.ParkingSpot.ValuePerHour;
+
+            //Adiciona o valor da locação no banco 
+            location.Value = valorLocacao;
+            if (_iLocationRepository.Update(location).Result.Success)
             {
-                if (_iLocationRepository.Disable(locacao.ID).Result.Success)
+                //Caso for adicionada com sucesso ela será desabilitada, pois o cliente saiu 
+                if (_iLocationRepository.Disable(location.ID).Result.Success)
                 {
+                    //Se o disable funcionar, adiociona a locação desse escopo e retorna 
                     response.Success = true;
+                    response.Data.Add(location);
                     return response;
                 }
+                //Caso o Disable falhe 
                 response.Errors.Add("Erro de fechamento de vaga!");
             }
+            //Caso o Update falhe
             else
             {
                 response.Errors.Add("Erro de Update de vaga!");
             }
-
+            //Retorna erros caso o Update tenha falhado
             return response;
            
           
