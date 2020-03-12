5ee436c6c5f77e0d675f881fa9b4b2441933a561
Arthur Martins <arthur.martins@hbsis.com.br>
2020-03-12 15:50:11 -0300

    controller

---
+++ PPParkingLot/Controllers/ClientController.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using BusinessLogicalLayer.Impl;
+using DAL.Context_EFCore_;
+using DataTransferObject;
+using Microsoft.AspNetCore.Mvc;
+using PPParkingLot.Models.Insert;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PPParkingLot.Controllers
+{
+    public class ClientController:Controller
+    {
+       
+
+    }
+
+}
