ac2d298a5f0ecc532122b5610a69956e88cca05d
gabrielarnovoltolini1945@gmail.com <gabrielarnovoltolini1945@gmail.com>
2020-03-12 14:53:15 -0300

    Método disable Location implementado

--- DAL/Repositories(EFCore)/LocationRepository.cs
+++ DAL/Repositories(EFCore)/LocationRepository.cs
@@ -67,9 +67,27 @@ namespace DataAccessLayer.Repositories_EFCore_
             }
         }
 
-        public Task<Response> Disable(int idLocation)
+        public async Task<Response> Disable(int idLocation)
         {
-            throw new NotImplementedException();
+            Response response = new Response();
+
+            try
+            {
+                using (var context = _context)
+                {
+                    LocationDTO location = await context.Locations.FindAsync(idLocation);
+                    location.IsActive = false;
+                    await context.SaveChangesAsync();
+                }
+                response.Success = true;
+                return response;
+            }
+            catch (Exception ex)
+            {
+                response.Errors.Add("Erro no banco de dados contate o administrador");
+                throw ex;
+
+            }
         }
 
       
