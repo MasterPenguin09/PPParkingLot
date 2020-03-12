cd10910eb7e7ef244e8ba5a93fd603ded5bab075
Nathalie Pfau <nana.pfau@gmail.com>
2020-03-12 14:30:38 -0300

    InsertViewModel completo acho

--- PPParkingLot/Models/Insert/ParkingSportInsertViewModel.cs
+++ PPParkingLot/Models/Insert/ParkingSportInsertViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +9,13 @@ namespace PPParkingLot.Models.Insert
 {
     public class ParkingSportInsertViewModel
     {
+        [DisplayName("ValuePerHour")]
+        [Required(ErrorMessage = "O vaor por hora deve ser informado")]
         public double ValuePerHour { get; set; }
+
+        [DisplayName("Type")]
+        [Required(ErrorMessage = "O Tipo deve ser informado")]
+        [StringLength(maximumLength: 200, MinimumLength = 3, ErrorMessage = "O tipo deve conter 3 e 200 caracteres")]
         public string Type { get; set; }
     }
 }
