222b61aec4b3bb50017a12a0b6a83f5902bdd0e1
Gabriel Voltolini <gabrielarnovolyolini1945@gmail.com>
2020-03-19 17:34:16 -0300

    ClientController pronto

--- PPParkingLot/Controllers/ClientController.cs
+++ PPParkingLot/Controllers/ClientController.cs
@@ -18,7 +18,122 @@ namespace PPParkingLot.Controllers
 {
     public class ClientController: BaseController
     {
-      
+        private readonly IClientService _service;
+        public ClientController(IClientService service)
+        {
+            this._service = service;
+        }
+
+
+        public async Task<ActionResult> Index()
+        {
+            DataResponse<ClientDTO> client = await _service.GetAll();
+
+            var configuration = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<ClientDTO, ClientInsertViewModel>();
+            });
+
+            IMapper mapper = configuration.CreateMapper();
+
+            List<ClientInsertViewModel> clientViewModel = mapper.Map<List<ClientInsertViewModel>>(client.Data);
+
+            ViewBag.Clients = clientViewModel;
+
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Register(ClientInsertViewModel viewModel)
+        {
+            Response response = new Response();
+
+            var configuration = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<ClientInsertViewModel, ClientDTO>();
+            });
+
+            IMapper mapper = configuration.CreateMapper();
+            ClientDTO dto = mapper.Map<ClientDTO>(viewModel);
+
+            response = await _service.Insert(dto);
+            //Se funcionou, redireciona pra página inicial
+            if (response.Success)
+            {
+                return RedirectToAction("Index", "Client");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = response.Errors;
+                return this.View();
+            }
+        }
+
+        public ActionResult Update()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Update(ClientInsertViewModel viewModel)
+        {
+            Response response = new Response();
+
+            var configuration = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<ClientInsertViewModel, ClientDTO>();
+            });
+
+            IMapper mapper = configuration.CreateMapper();
+            ClientDTO dto = mapper.Map<ClientDTO>(viewModel);
+
+            response = await _service.Update(dto);
+            //Se funcionou, redireciona pra página inicial
+            if (response.Success)
+            {
+                return RedirectToAction("Index", "Client");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = response.Errors;
+                return this.View();
+            }
+        }
+
+        public ActionResult Disable()
+        {
+            return this.View();
+        }
+        public async Task<ActionResult> Disable(ClientInsertViewModel viewModel)
+        {
+            Response response = new Response();
+
+            var configuration = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<ClientInsertViewModel, ClientDTO>();
+            });
+
+            IMapper mapper = configuration.CreateMapper();
+
+            ClientDTO dto = mapper.Map<ClientDTO>(viewModel);
+            response = await _service.Disable(dto.ID);
+            //Se funcionou, redireciona pra página inicial
+            if (response.Success)
+            {
+                return RedirectToAction("Index", "Client");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = response.Errors;
+                return this.View();
+            }
+
+        }
 
     }
 
