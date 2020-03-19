82cb3e6896b501770accf294015cef0f20a0c28a
Gabriel Voltolini <gabrielarnovolyolini1945@gmail.com>
2020-03-19 17:35:17 -0300

    Employee controller pronto

--- PPParkingLot/Controllers/EmployeeController.cs
+++ PPParkingLot/Controllers/EmployeeController.cs
@@ -16,7 +16,121 @@ namespace PPParkingLot.Controllers
 {
     public class EmployeeController : BaseController
     {
-      
+        private readonly IEmployeeService _service;
+        public EmployeeController(IEmployeeService service)
+        {
+            this._service = service;
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            DataResponse<EmployeeDTO> employee = await _service.GetAll();
+
+            var configuration = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<EmployeeDTO, EmployeeInsertViewModel>();
+            });
+
+            IMapper mapper = configuration.CreateMapper();
+
+            List<EmployeeInsertViewModel> empViewModel = mapper.Map<List<EmployeeInsertViewModel>>(employee.Data);
+
+            ViewBag.Employees = empViewModel;
+
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Register(EmployeeInsertViewModel viewModel)
+        {
+            Response response = new Response();
+
+            var configuration = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<EmployeeInsertViewModel, EmployeeDTO>();
+            });
+
+            IMapper mapper = configuration.CreateMapper();
+            EmployeeDTO dto = mapper.Map<EmployeeDTO>(viewModel);
+
+            response = await _service.Insert(dto);
+            //Se funcionou, redireciona pra página inicial
+            if (response.Success)
+            {
+                return RedirectToAction("Index", "Employee");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = response.Errors;
+                return this.View();
+            }
+        }
+
+        public ActionResult Update()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Update(EmployeeInsertViewModel viewModel)
+        {
+            Response response = new Response();
+
+            var configuration = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<EmployeeInsertViewModel, EmployeeDTO>();
+            });
+
+            IMapper mapper = configuration.CreateMapper();
+            EmployeeDTO dto = mapper.Map<EmployeeDTO>(viewModel);
+
+            response = await _service.Update(dto);
+            //Se funcionou, redireciona pra página inicial
+            if (response.Success)
+            {
+                return RedirectToAction("Index", "Employee");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = response.Errors;
+                return this.View();
+            }
+        }
+
+        public ActionResult Disable()
+        {
+            return this.View();
+        }
+        public async Task<ActionResult> Disable(EmployeeInsertViewModel viewModel)
+        {
+            Response response = new Response();
+
+            var configuration = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<EmployeeInsertViewModel, EmployeeDTO>();
+            });
+
+            IMapper mapper = configuration.CreateMapper();
+
+            EmployeeDTO dto = mapper.Map<EmployeeDTO>(viewModel);
+            response = await _service.Disable(dto.ID);
+            //Se funcionou, redireciona pra página inicial
+            if (response.Success)
+            {
+                return RedirectToAction("Index", "Employee");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = response.Errors;
+                return this.View();
+            }
+
+        }
 
     }
 }
