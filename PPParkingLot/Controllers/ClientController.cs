5f62c9b02f799ee244c393f6b4cbf31dd6aca916
Gabriel Voltolini <gabrielarnovolyolini1945@gmail.com>
2020-03-19 16:55:44 -0300

    ClientController pronto

--- PPParkingLot/Controllers/ClientController.cs
+++ PPParkingLot/Controllers/ClientController.cs
@@ -27,9 +27,7 @@ namespace PPParkingLot.Controllers
 
         public async Task<ActionResult> Index()
         {
-
-
-            DataResponse<ClientDTO> brands = await _service.GetAll();
+            DataResponse<ClientDTO> client = await _service.GetAll();
 
             var configuration = new MapperConfiguration(cfg =>
             {
@@ -38,40 +36,103 @@ namespace PPParkingLot.Controllers
 
             IMapper mapper = configuration.CreateMapper();
 
-            List<ClientInsertViewModel> clientsViewModel =
-            mapper.Map<List<ClientInsertViewModel>>(brands.Data);
-            ViewBag.Clients = clientsViewModel;
+            List<ClientInsertViewModel> clientViewModel = mapper.Map<List<ClientInsertViewModel>>(client.Data);
+
+            ViewBag.Clients = clientViewModel;
 
             return View();
         }
 
         public ActionResult Register()
         {
-            return View();
+            return this.View();
         }
 
         [HttpPost]
         public async Task<ActionResult> Register(ClientInsertViewModel viewModel)
         {
+            Response response = new Response();
+
             var configuration = new MapperConfiguration(cfg =>
             {
                 cfg.CreateMap<ClientInsertViewModel, ClientDTO>();
             });
+
+            IMapper mapper = configuration.CreateMapper();
+            ClientDTO dto = mapper.Map<ClientDTO>(viewModel);
+
+            response = await _service.Insert(dto);
+            //Se funcionou, redireciona pra página inicial
+            if (response.Success)
+            {
+                return RedirectToAction("Index", "Client");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = response.Errors;
+                return this.View();
+            }
+        }
+
+        public ActionResult Update()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Update(ClientInsertViewModel viewModel)
+        {
+            Response response = new Response();
+
+            var configuration = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<ClientInsertViewModel, ClientDTO>();
+            });
+
+            IMapper mapper = configuration.CreateMapper();
+            ClientDTO dto = mapper.Map<ClientDTO>(viewModel);
+
+            response = await _service.Update(dto);
+            //Se funcionou, redireciona pra página inicial
+            if (response.Success)
+            {
+                return RedirectToAction("Index", "Client");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = response.Errors;
+                return this.View();
+            }
+        }
+
+        public ActionResult Disable()
+        {
+            return this.View();
+        }
+        public async Task<ActionResult> Disable(ClientInsertViewModel viewModel)
+        {
+            Response response = new Response();
+
+            var configuration = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<ClientInsertViewModel, ClientDTO>();
+            });
+
             IMapper mapper = configuration.CreateMapper();
 
             ClientDTO dto = mapper.Map<ClientDTO>(viewModel);
-
-
-            try
+            response = await _service.Disable(dto.ID);
+            //Se funcionou, redireciona pra página inicial
+            if (response.Success)
             {
-                await _service.Insert(dto);
-                return RedirectToAction("Index", "Clients");
+                return RedirectToAction("Index", "Client");
             }
-            catch (Exception ex)
+            else
             {
-                ViewBag.Erros = ex.Message;
+                ViewBag.ErrorMessage = response.Errors;
+                return this.View();
             }
-            return View();
+
         }
 
     }
