f92690f595b3ac98b9676d382943b5f28bbfb35b
Gabriel Voltolini <gabrielarnovolyolini1945@gmail.com>
2020-03-19 17:38:11 -0300

    ParkingSpot controller pornto

--- PPParkingLot/Controllers/ParkingSpotController.cs
+++ PPParkingLot/Controllers/ParkingSpotController.cs
@@ -14,7 +14,132 @@ namespace PPParkingLot.Controllers
 {
     public class ParkingSpotController: BaseController
     {
-    
+        private readonly IParkingSpotService _service;
+        public ParkingSpotController(IParkingSpotService service)
+        {
+            this._service = service;
+        }
+
+
+        public async Task<ActionResult> Index()
+        {
+            DataResponse<ParkingSpotDTO> parkingSpot = await _service.GetAll();
+
+            var configuration = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<ParkingSpotDTO, ParkingSpotInsertViewModel>();
+            });
+
+            IMapper mapper = configuration.CreateMapper();
+
+            if (parkingSpot.Success)
+            {
+                List<ParkingSpotInsertViewModel> ParkingSpotsViewModel = mapper.Map<List<ParkingSpotInsertViewModel>>(parkingSpot.Data);
+                ViewBag.ParkingSpots = ParkingSpotsViewModel;
+            }
+            else
+            {
+                ViewBag.ParkingSpots = parkingSpot;
+
+            }
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Register(ParkingSpotInsertViewModel viewModel)
+        {
+            Response response = new Response();
+            var configuration = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<ParkingSpotInsertViewModel, ParkingSpotDTO>();
+            });
+
+            IMapper mapper = configuration.CreateMapper();
+
+            ParkingSpotDTO dto = mapper.Map<ParkingSpotDTO>(viewModel);
+
+            response = await _service.Insert(dto);
+            //Se funcionou, redireciona pra página inicial
+            if (response.Success)
+            {
+                return RedirectToAction("Index", "ParkingSpot");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = response.Errors;
+                return this.View();
+            }
+        }
+
+
+        public ActionResult Update()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Update(ParkingSpotInsertViewModel viewModel)
+        {
+            Response response = new Response();
+
+            var configuration = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<ParkingSpotInsertViewModel, ParkingSpotDTO>();
+            });
+
+            IMapper mapper = configuration.CreateMapper();
+
+            ParkingSpotDTO dto = mapper.Map<ParkingSpotDTO>(viewModel);
+
+            response = await _service.Update(dto);
+            //Se funcionou, redireciona pra página inicial
+            if (response.Success)
+            {
+                return RedirectToAction("Index", "ParkingSpot");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = response.Errors;
+                return this.View();
+            }
+
+        }
+
+
+        public ActionResult Disable()
+        {
+            return this.View();
+        }
+        public async Task<ActionResult> Disable(ParkingSpotInsertViewModel viewModel)
+        {
+            Response response = new Response();
+
+            var configuration = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<ParkingSpotInsertViewModel, ParkingSpotDTO>();
+            });
+
+            IMapper mapper = configuration.CreateMapper();
+
+            ParkingSpotDTO dto = mapper.Map<ParkingSpotDTO>(viewModel);
+            response = await _service.Disable(dto.ID);
+            //Se funcionou, redireciona pra página inicial
+            if (response.Success)
+            {
+                return RedirectToAction("Index", "ParkingSpot");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = response.Errors;
+                return this.View();
+            }
+
+        }
     }        
     
 }
