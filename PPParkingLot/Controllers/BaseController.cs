12a2b8f35b18d1b126ed52512d722746595797e1
Gabriel Voltolini <gabrielarnovolyolini1945@gmail.com>
2020-03-23 17:57:14 -0300

    ControllerLogin Feito

--- PPParkingLot/Controllers/BaseController.cs
+++ PPParkingLot/Controllers/BaseController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataTransferObject.ComplexTypes;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,15 +11,42 @@ namespace PPParkingLot.ControllersView
 {
     public class BaseController : Controller
     {
-        //Antes de qualquer execução de ação, este método é rodado ^_^
+        //protected bool IsManager { get; set; }
+        //protected bool IsEmployee { get; set; }
+        //protected bool IsClient { get; set; }
+
+        ////Antes de qualquer execução de ação, este método é rodado ^_^
         //public override void OnActionExecuting(ActionExecutingContext filterContext)
         //{
-        //    var cookie = this.Request.Cookies["NomeDoCookie"];
+        //    //    var cookie = this.Request.Cookies["NomeDoCookie"];
+
+        //    var cookie = this.Request.Cookies["MyAccount_SmartParking"];
+
 
         //    if (cookie == null)
         //    {
         //        filterContext.Result = new RedirectResult(Url.Action("Login", "Home"));
+
         //    }
+        //    else
+        //    {
+        //        UserPattern userFromCookie = new UserPattern();
+        //        userFromCookie = (UserPattern)JsonConvert.DeserializeObject(cookie);
+
+        //        if (userFromCookie.AccessLevel.Equals(DataTransferObject.Enums.EAccessLevel.Manager))
+        //        {
+        //            this.IsManager = true;
+        //        }
+        //        else if (userFromCookie.AccessLevel.Equals(DataTransferObject.Enums.EAccessLevel.Employee))
+        //        {
+        //            this.IsEmployee = true;
+        //        }
+        //        else if (userFromCookie.AccessLevel.Equals(DataTransferObject.Enums.EAccessLevel.Client))
+        //        {
+        //            this.IsClient = true;
+        //        }
+        //    }
+
         //    base.OnActionExecuting(filterContext);
         //}
     }
